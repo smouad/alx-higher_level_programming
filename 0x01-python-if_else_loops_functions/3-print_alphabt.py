95a501bd624094c5c16070c4bed98f4de1bf2ed1
smouad <sodormouad@gmail.com>
2024-01-02 21:18:54 +0100

    print al

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if i == ord('q') or i == ord('e'):
-        i -= 32;
-    print("{:s}".format(chr(i)), end="")
+    if i != ord('q') and i != ord('e'):
+        print("{:s}".format(chr(i)), end="")
