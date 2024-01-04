be86c0559da455158c6c303fd4e7ae5cd14b885f
smouad <sodormouad@gmail.com>
2024-01-04 20:34:10 +0100

    hiden

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,9 @@
 if __name__ == "__main__":
     import sys
 
-total = 0
-for i in range(len(sys.argv) - 1):
-    total += int(sys.argv[i + 1])
-print("{}".format(total))
+argc = len(sys.argv) - 1
+res = 0
+
+for i in range(argc):
+    res += int(sys.argv[i + 1])
+print("{}".format(res))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+dirs = dir(hidden_4)
+for d in dirs:
+    if d[:2] != "__":
+        print(name)
