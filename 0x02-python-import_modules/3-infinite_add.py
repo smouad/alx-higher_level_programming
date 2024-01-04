cd7e53d991fe9b3a835bc4e2c867c09cbf6ad70f
smouad <sodormouad@gmail.com>
2024-01-04 20:15:22 +0100

    infinit sum

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+argc = len(sys.argv) - 1
+res = 0
+
+for i in range(argc):
+    res += int(sys.argv[i + 1])
+print("{}",format(res))
