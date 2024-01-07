08e7aac0863a2f4c39741ec2fde0b80606b36012
smouad <sodormouad@gmail.com>
2024-01-07 22:30:34 +0100

    only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    ret = ()
+    for i in my_list:
+        if i % 2 == 0:
+            ret += (True,)
+        else:
+            ret += (False,)
+    return ret
