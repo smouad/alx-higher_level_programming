bbd0c2971455e5c464c18ab17d176302b2493c45
smouad <sodormouad@gmail.com>
2024-01-07 22:25:32 +0100

    fix max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return "None"
+        return None
     max_int = 0
     for i in my_list:
         if i > max_int:
             max_int = i
     return max_int
+
+my_list = []
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
