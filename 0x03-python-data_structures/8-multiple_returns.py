707e2c89ffc9f6f5821c967b2e9f4ff2069a2c38
smouad <sodormouad@gmail.com>
2024-01-07 22:08:33 +0100

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    my_tupple = ()
     if len(snetence) == 0:
-        return (0, "None")
-    return (len(sentence), sentence[0])
+        my_tupple = (0, "None")
+    else:
+        my_tupple = (len(sentence), sentence[0])
+    return my_tupple
