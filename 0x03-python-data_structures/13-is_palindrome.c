d77e8a3ed5a28c37e8703b218fcfc5a6e63be00e
smouad <sodormouad@gmail.com>
2024-01-08 18:39:56 +0100

    palindrom

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * is_palindrome - check if a linked list is a palindrome
  * @head: first node
@@ -8,23 +7,22 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *tmp = *head;
-	int *values, l_len = 0, cLoop, limit;
+	int *values, i = 0, cLoop, limit;
 
 	if (head == NULL || *head == NULL)
 		return (1);
 
 	while (tmp != NULL)
 	{
-		values[l_len] = tmp->n;
-		l_len++;
+		values[i] = tmp->n;
+		i++;
 		tmp = tmp->next;
 	}
-
-	values = malloc(l_len * sizeof(int));
-	limit = (l_len / 2 == 0) ? l_len / 2 : (l_len + 1) \ 2;
+	values = (int *)malloc(i * sizeof(int));
+	limit = (i % 2 == 0) ? i / 2 : (i + 1) / 2;
 
 	for (cLoop = 0; cLoop < limit; cLoop++)
-		if (values[cLoop] != values[l_len - 1 - cLoop])
+		if (values[cLoop] != values[i - 1 - cLoop])
 			return (free(values), 0);
 
 	return (free(values), 1);
