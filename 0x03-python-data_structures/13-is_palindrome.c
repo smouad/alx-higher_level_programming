282c96b8835ea0b0dda4faee583c7d27354bc726
smouad <sodormouad@gmail.com>
2024-01-08 18:38:18 +0100

    palindrom

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,23 +8,24 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *tmp = *head;
-	int values[2048], i = 0, cLoop, limit;
+	int *values, l_len = 0, cLoop, limit;
 
 	if (head == NULL || *head == NULL)
 		return (1);
 
 	while (tmp != NULL)
 	{
-		values[i] = tmp->n;
-		i++;
+		values[l_len] = tmp->n;
+		l_len++;
 		tmp = tmp->next;
 	}
 
-	limit = (i % 2 == 0) ? i / 2 : (i + 1) / 2;
+	values = malloc(l_len * sizeof(int));
+	limit = (l_len / 2 == 0) ? l_len / 2 : (l_len + 1) \ 2;
 
 	for (cLoop = 0; cLoop < limit; cLoop++)
-		if (values[cLoop] != values[i - 1 - cLoop])
-			return (0);
+		if (values[cLoop] != values[l_len - 1 - cLoop])
+			return (free(values), 0);
 
-	return (1);
+	return (free(values), 1);
 }
