1b6f2c99d3be749b59cd85db158d0810cc896e10
smouad <sodormouad@gmail.com>
2024-01-10 22:56:56 +0100

    not in

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    new_set = set()
+    for elem in set_1:
+        if elem not in set_2:
+            new_set.add(elem)
+    for elem in set_2:
+        if elem not in set_1:
+            new_set.add(elem)
+    return new_set
