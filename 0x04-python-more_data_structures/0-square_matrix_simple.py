ad90a03b468eceaf3fe2f11a7c6cd66c712e55b6
smouad <sodormouad@gmail.com>
2024-01-10 22:29:37 +0100

    matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import math
+def square_matrix_simple(matrix=[]):
+    ret_matrix = []
+    for i, rows in enumerate(matrix):
+        ret_row = []
+        for j, col in enumerate(rows):
+            ret_row.append(col ** 2)
+        ret_matrix.append(ret_row)
+    return ret_matrix
