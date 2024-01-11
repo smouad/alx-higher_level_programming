d1f5edd6673360aa434485aafeb38476fea5bc29
smouad <sodormouad@gmail.com>
2024-01-11 19:49:29 +0100

    roman numbers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    res = 0
+    my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+               'C': 100, 'D': 500, 'M': 1000}
+    for num in roman_string:
+        res += my_dict[num]
+    return res
