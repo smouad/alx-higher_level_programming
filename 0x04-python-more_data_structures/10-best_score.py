433ffdf2c55af3c2c71d1557c9941c0018468516
smouad <sodormouad@gmail.com>
2024-01-11 19:11:17 +0100

    find best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    score = 0
+    for key in a_dictionary:
+        if a_dictionary[key] > score:
+            score = a_dictionary[key]
+    for key, value in a_dictionary.items():
+        if value == score:
+            return key
+    return None
