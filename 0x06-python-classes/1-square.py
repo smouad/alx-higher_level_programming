41d768cdf60e1f262e0e52beef883c213d36f668
smouad <sodormouad@gmail.com>
2024-01-25 22:04:18 +0100

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
-"""define a square"""
+"""define a class Square."""
 
 
 class Square:
+    """Represent a square."""
+
     def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new Square
+        """
         self.__size = size
