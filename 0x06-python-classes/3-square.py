508cd03c5874954806f3b507298ed53038889917
smouad <sodormouad@gmail.com>
2024-01-25 22:26:34 +0100

    area update

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,6 +17,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Return the current square area"""
         return self.__size * self.__size
