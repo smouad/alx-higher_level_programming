4dee637e4a7958001f79ca6f574da9daa3ccf208
smouad <sodormouad@gmail.com>
2024-01-25 22:15:07 +0100

    raise

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""Define a class Square"""
+
+class Square:
+    """represent a square"""
+
+    def __init__(self, size=0):
+        """initializind the class"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
