2b3c7046c29f3d883f2e69bc4abf628ab2cea3af
smouad <sodormouad@gmail.com>
2024-02-04 12:31:45 +0100

    typo

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,13 +4,13 @@
 
 
 class Rectangle:
-    """Represent a rectangle
-    
+    """Represent a rectangle.
+
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
     """
 
-    number_of_instance = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
@@ -19,7 +19,7 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-        type(self).number_of_instance += 1
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
@@ -82,5 +82,5 @@ class Rectangle:
 
     def __del__(self):
         """print a message when deleting the rectangle"""
-        type(self).number_of_instance -= 1
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
