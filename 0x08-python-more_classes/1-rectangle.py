2b87cf9ca11fb36197028a0aaecbaa21a002b8a3
smouad <sodormouad@gmail.com>
2024-02-04 12:00:27 +0100

    style

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """gets the height of the rectangle"""
