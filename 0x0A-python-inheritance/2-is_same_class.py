98a1579ef35a2216a5e65d602c2144e85dc852de
mouad sodor <sodormouad@gmail.com>
2024-02-25 16:11:28 +0100

    lsat

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,4 +6,4 @@ def is_same_class(obj, a_class):
     """Return true if object is an instance of the
     class, otherwise return false
     """
-    return isinstance(obj, a_class)
+    return type(obj) == a_class:
