3962fe0ee4158f968163f61dd1052eb874893b44
mouad sodor <sodormouad@gmail.com>
2024-02-25 14:51:14 +0100

    push 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
 
 class Mylist(list):
     """A class that inherits from list."""
     def print_sorted(self):
+        """prints the sorted list"""
         print(sorted(self))
     
