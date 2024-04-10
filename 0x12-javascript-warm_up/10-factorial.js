2dc003e7ecdb58acf7a332f335faf4585dec215a
mouad sodor <sodormouad@gmail.com>
2024-04-10 20:11:27 +0000

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const num = Math.floor(Number(process.argv[2]));
+
+function factorial (num) {
+  if (num === 0 || isNaN(num)) { return 1; } else {
+    return num * factorial(num - 1);
+  }
+}
+
+console.log(factorial(num));
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+const num1 = Math.floor(Number(process.argv[2]));
+const num2 = Math.floor(Number(process.argv[3]));
+
+const res = num1 + num2;
+
+console.log(res);
