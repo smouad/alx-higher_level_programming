1611c94742d9d4cb94f20040c9aef50b1d5c13ea
mouad sodor <sodormouad@gmail.com>
2024-04-10 18:54:30 +0000

    all in one

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const rep = Math.floor(Number(process.argv[2]));
+
+for (let i = 0; i < rep; i++) {
+  if (isNaN(rep)) {
+    console.log('Missing number of occurrences');
+    break;
+  }
+  console.log('C is fun');
+}
