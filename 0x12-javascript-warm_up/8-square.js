d0c9aa02c854ba9aea11dd9cd6a3ed3d2fb122df
mouad sodor <sodormouad@gmail.com>
2024-04-10 19:24:45 +0000

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const size = Math.floor(Number(process.argv[2]));
+
+for (let i = 0; i < size; i++) {
+  if (isNaN(size)) {
+    console.log('Missing size');
+    break;
+  }
+  for (let j = 0; j < size; j++) {
+    console.log('X');
+  }
+}
